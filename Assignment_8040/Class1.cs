faf70027b5759916c0e9440f2f94ec7e83765d4b
Navneet Singh <Nsingh5152@conestogac.on.ca>
2019-05-28 22:19:22 -0400

    Add project files.

---
+++ Assignment_8040/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8040
+{
+    public class Class1
+    {
+    }
+}
